8d9ab3a0f9ff06b092775b12de7fe3581a1b226c
takeo <yass@takibi.net>
2020-06-14 11:30:48 +0900

    Add rotate skew re-balance

--- loader/ocr.py
+++ loader/ocr.py
@@ -4,7 +4,7 @@ import pyocr
 
 WHITE_LEVEL = 150
 BLANK_LINE_LEVEL = 5
-PADDING = 2
+PADDING = 1
 HEADER_LINES = 2
 FOOTER_LINES = 1
 
@@ -15,11 +15,54 @@ class Reader:
         self.img_lines = []
         self.col = []
 
+
+    def rotate_degree(self):
+        start_x, end_x = self.chop_lines()
+
+        rad = (start_x - end_x) / 3000
+
+        return rad * 180
+
     def chop_lines(self):
+        start_x = 0
+        end_x = 0
+
         for index in range(HEADER_LINES, len(self.img_lines)-FOOTER_LINES):
             img = self.img_lines[index]
             c = self.chop_colums(img)
             self.col.append(c)
+
+            if c[0] < 100:
+                if start_x == 0:
+                    start_x = c[0]
+                else:
+                    end_x = c[0]
+
+        return start_x, end_x
+
+    def split_image(self):
+
+        index_line = self.img[0:1, :]
+        other_line = self.img[0:1, :]
+
+        for index in range(HEADER_LINES, len(self.img_lines)-FOOTER_LINES):
+            img = self.img_lines[index]
+            c = self.chop_colums(img)
+
+            in_index = False
+            if c[0] < 100:
+                if not in_index:
+                    index_line = np.r_[index_line, img]
+                    in_index = True
+                else:
+                    other_line = np.r_[other_line, img]
+            else:
+                in_index = False
+                other_line = np.r_[other_line, img]
+
+            self.col.append(c)
+
+        return index_line, other_line
 
     def chop_colums(self, img):
         img = np.where(WHITE_LEVEL < img, 255, img)
@@ -42,7 +85,7 @@ class Reader:
                 if in_char:
                     end = pos
                     width = end - start
-                    if 10 < width:
+                    if 6 < width:
                         char_pos.append(pos)
 
                 in_char = False
@@ -65,9 +108,12 @@ class Reader:
 
         return res
 
-    def open(self, file):
+    def open(self, file, r=0):
+        self.img_lines = []
+        self.col = []
+
         img = Image.open(file)
-        img = img.rotate(0, fillcolor=(255, 255, 255))
+        img = img.rotate(r, fillcolor=(255, 255, 255))
         img = np.array(img.convert('L'))
         self.img = img
         img = np.where(WHITE_LEVEL < img, 255, img)
--- test/ocr_test.py
+++ test/ocr_test.py
@@ -1,19 +1,62 @@
 import unittest
 from loader.ocr import Reader
-
+from PIL import Image
 
 class MyTestCase(unittest.TestCase):
     def test_load(self):
         reader = Reader()
         reader.open('../TESTDATA/light.png')
 
+        r = reader.rotate_degree()
+        print(r)
+
+        reader.open('../TESTDATA/light.png', r)
+        r = reader.rotate_degree()
+        print(r)
+
+
+
     def test_col(self):
         reader = Reader()
         reader.open('../TESTDATA/light.png')
-        reader.chop_lines()
+        start_x, end_x = reader.chop_lines()
+
+        print(start_x, end_x)
 
         for c in reader.col:
             print(c)
+
+    def test_split_image(self):
+        reader = Reader()
+        reader.open('../TESTDATA/light.png')
+
+        r = reader.rotate_degree()
+        reader.open('../TESTDATA/light.png', r)
+
+        index, other = reader.split_image()
+
+        i = reader.chop_colums(index)
+        print(i)
+        o = reader.chop_colums(other)
+        print(o)
+
+        for pos in i:
+            index[:, pos] = 200
+
+        for pos in o:
+            other[:, pos] = 200
+
+        img = Image.fromarray(index)
+        img.save('index.png')
+
+        img = Image.fromarray(other)
+        img.save('other.png')
+
+        i = reader.chop_colums(index)
+        print(i)
+        o = reader.chop_colums(other)
+        print(o)
+
 
     def test_chop(self):
         reader = Reader()
